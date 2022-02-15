c57c8b6b3b8a1d47e0beaa660d75af59579e2a30
Paulina Rutkowska <polciaalicious@gmail.com>
2022-02-15 17:08:40 +0100

    add javascript

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-import "../scss/modules/main.scss";
+// import "../scss/modules/main.scss";
 
-console.log ("Hi, I'm Paulina, how are you?")
+console.log ("Hey, are you checking the console to see if there are any errors? There are no ;) Have a nice day!")
 
-fetch('https://api.github.com/users/paurut/repos?sort=created&&direction=asc')
-.then((res) => res.json())
-.then((res) => {
-    console.log(res);
-})
-.catch((e) => console.log(e));
+// fetch('https://api.github.com/users/paurut/repos?sort=created&&direction=asc')
+// .then((res) => res.json())
+// .then((res) => {
+//     console.log(res);
+// })
+// .catch((e) => console.log(e));
